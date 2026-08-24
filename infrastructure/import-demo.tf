ba8321e369b3549157ef42ddd080aabcfb8d2634
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2026-08-24 13:59:42 +0100

    Update import-demo.tf

--- infrastructure/import-demo.tf
+++ infrastructure/import-demo.tf
@@ -17,3 +17,10 @@ import {
   to = module.managed_redis.azurerm_resource_group.rg[0]
   id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/hmc-admin-ui-demo-rg"
 }
+
+import {
+  for_each = local.demo_resource_group_import
+
+  to = module.application_insights.azurerm_application_insights.app_insights
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/hmc-admin-ui-demo/providers/Microsoft.Insights/components/hmc-admin-ui-appinsights-demo"
+}
