544a315025be4685aa4bd4077efd5d5e69fb59fa
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2026-09-02 13:50:07 +0100

    Task/fix master build (#126)
    * Update package.json
    
    * Add test changes to fix Sonar

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  roots: ['<rootDir>/src/test/unit'],
-  testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
+  roots: ['<rootDir>/src'],
+  testRegex: '/src/test/unit/.*\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
+  collectCoverageFrom: [
+    'src/main/**/*.{ts,js}',
+    '!src/main/app.ts',
+    '!src/main/development.ts',
+    '!src/main/index.js',
+    '!src/main/server.ts',
+    '!src/main/modules/nunjucks/*.ts',
+    '!src/main/types/**/*.ts',
+    '!src/main/public/**',
+    '!src/main/views/**',
+  ],
 };
